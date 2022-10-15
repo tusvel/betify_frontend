d0096b24fe6b0b2daf1dfd38ad92b1c9d89eea22
tusvel <tusvel@yandex.ru>
2022-10-15 20:13:52 +0300

    Configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import '@/assets/styles/globals.scss';
+import { DevSupport } from '@react-buddy/ide-toolbox';
+import { ComponentPreviews, useInitial } from '../dev';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DevSupport
+      ComponentPreviews={ComponentPreviews}
+      useInitialHook={useInitial}
+    >
+      <Component {...pageProps} />
+    </DevSupport>
+  );
 }
 
-export default MyApp
+export default MyApp;
